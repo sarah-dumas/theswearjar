598515485007f9c64c00ddafff6d79e5a94b8dc5
itvm26-5 <sysadmin@itvm26-5.(none)>
2013-11-06 13:55:47 -0800

    Fleshed out more of nfs.hs. Please check on why apt_get is not working and how to implement it cleanly.

--- network/nfs.hs
+++ network/nfs.hs
@@ -9,34 +9,37 @@ default (T.Text)
 sudo_ com args = run_ "sudo" (com:args)
 
 --update the system and install nfs and autofs
-
-apt_get "update"[]
-apt_get "install" ["nfs-common"]
-apt_get "install" ["nfs-kernel-server]
+main = shelly $ verbosely $ do
+       apt_get "update"[]
+       apt_get "install" ["nfs-common"]
+       apt_get "install" ["nfs-kernel-server"]
 
 
 --move directories from home to home.computername
 
-computername <- readfile "computername.config"
-run_ "mv" ["/home", "/home." T.append computername]
+directoryMove = do computername <- readfile "computername.config"
+                   run_ "mv" ["/home", "/home." T.append computername]
 
 
 --create a directory to use as a mount point named /home
 
-run_ "mkdir" ["/home"]
+makeDir = do run_ "mkdir" ["/home"]
 
 
 
---edit /etc/passwd to change sysadmin account's home to home.computername
+--append /etc/passwd to change sysadmin account's home to home.computername
 
 
 
 --append /etc/exports to tell it where and how to export our homes
 
+exporter = do exports <- readfile "exports.config"
+              appendfile "/etc/exports" exports
 
 
 --edit /etc/auto.master to tell it that the homes are located in auto.home
 
+appender = do appendfile "/etc/auto.master" "/home /etc/auto.home"
 
 
 --restart the virtual machine
