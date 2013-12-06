9b3e0ad86016b91021302ca06d1fa0423b36156f
Sarah Dumas <sarahdumas@elliot-sterns-macbook-pro.local>
2013-12-05 19:46:13 -0500

    Fully functional twitter bot. :wq

---
+++ twitterplaytime/python/theswearjar.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+#imports
+import tweepy, time
+import settings
+
+#globals
+REPLY_LIST = []
+SWEARS_LIST = []
+
+ 
+def IDsetter(statefile,id):
+
+    last_id=IDgetter(statefile)
+    f=open(statefile,'w')
+    f.write(str(id))
+    f.close()
+
+def IDgetter(statefile):
+
+    f=open(statefile,'r')
+    id = f.readlines()
+    for line in f:      
+        f = int(line)
+        return id
+    f.close()
+    #return id
+
+def listLoader():
+
+    global REPLY_LIST
+    REPLY_LIST = [line.lower().strip() for line in open('results.txt')]
+
+    global SWEARS_LIST
+    SWEARS_LIST = [line.lower().strip() for line in open('swears.txt')]
+
+def replyToTweet(api,reply):
+   
+    normalized_tweet = reply.text.lower().strip()
+
+    if normalized_tweet.split().count('@'+ reply.user.screen_name.lower()) > 0:
+        return
+    
+    return api.update_status("@{0} put a quarter in the swear jar: http://theswearjar.weebly.com".format(reply.user.screen_name.lower()))
+    time.sleep(350);
+
+def main():
+
+
+    CONSUMER_KEY = 'cxCjlSLtIR2Lp5SwcEaXQ'
+    CONSUMER_SECRET = 'BziDhlF6qeaWb505MjbuPbIdsoAYzzAzybnSeXFVb18'
+    ACCESS_KEY = '2227279891-UbAFtiT5NpGu5OKFgjqQe9UANGAibaowRNkBsnY'
+    ACCESS_SECRET = 'NRADw3kJM6JDX3Nw72r5WUUQdW8lvKuXVEg8sFblAyOSF'
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+    api = tweepy.API(auth)
+
+    listLoader()
+
+
+    last_id=IDgetter(settings.last_id_file)
+    
+    friends = api.friends_ids()
+
+    for x in SWEARS_LIST:
+
+        replies = api.search(q="cunt")
+        replies.reverse()
+
+        for reply in replies:
+    
+            replyToTweet(api,reply)
+
+main()
